862a55d7c4a32663461ab92d2974fee847fa8716
Amna N <amnanaqvi6110@gmail.com>
2025-12-12 17:20:12 -0500

    Assignment title, due date, and name; Part 1 header, question 1 solution

---
+++ AMNA_NAQVI_IntermediateR_Assignment.R
@@ -0,0 +1,9 @@
+# INTERMEDIATE R ASSIGNMENT
+# DUE - DEC 12 2025
+# Amna Naqvi
+
+############ PART 1 - BASICS AND TIDYVERSE ESSENTIALS ############
+# 1) installing package and saving data to 'movies'
+install.packages("ggplot2movies")
+library(ggplot2movies)
+movies <- ggplot2movies::movies
